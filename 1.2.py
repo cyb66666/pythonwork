87c5abe4bbac7464f64ece84a4a5efbc1a22354c
cyb <1411972815@qq.com>
2023-10-05 17:39:58 +0800

    finish

--- 1.2.py
+++ 1.2.py
@@ -1,3 +1,4 @@
+#输入三个整数x,y,z，请尝试用多种方式把这三个数由大到小输出
 a,b,c=input().split()
 n=[a,b,c]
 n.sort(reverse=True)
--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
+#输入三个整数x,y,z，请尝试用多种方式把这三个数由大到小输出
 a,b,c=input().split()
-
 if a<b:
     temp=a
     a=b
